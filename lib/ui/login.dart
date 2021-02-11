65a0bfab3e50dad08b28d5d55410728979c7a1aa
Tejas Vedantham <tejas6080@gmail.com>
2021-02-11 00:07:15 -0500

    Added to login UI

--- lib/ui/login.dart
+++ lib/ui/login.dart
@@ -24,59 +24,115 @@ class LoginWidget extends StatelessWidget {
       backgroundColor: Colors.blue[400],
       body: Column(
         children: [
-          Container(
-            margin: EdgeInsets.all(10),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text("Hubble", style: TextStyle(color: Colors.white, fontSize: 48)),
-                Image.asset('assets/logo.png'),
-                TextField(
-                  decoration: InputDecoration(
-                    fillColor: Colors.white,
-                    filled: true,
-                    hintText: 'Email',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10)
-                    )
-                  ),
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                    fillColor: Colors.white,
-                    filled: true,
-                    hintText: 'Password',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10)
-                    )
-                  ),
-                ),
-                ButtonTheme(
-                  minWidth: 150,
-                  height: 50,
-                  child: RaisedButton(
-                    elevation: 10,
-                    color: Colors.white,
-                    padding: EdgeInsets.all(10),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10)
+          Expanded(
+            child: Container(
+              margin: EdgeInsets.all(20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text("Hubble", style: TextStyle(color: Colors.white, fontSize: 48)),
+                  Image.asset('assets/logo.png'),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                    child: Opacity(
+                      opacity: 0.4,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          fillColor: Colors.grey[100],
+                          filled: true,
+                          hintText: 'Email',
+                          hintStyle: TextStyle(color: Colors.grey[800]),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5),
+                            borderSide: BorderSide.none
+                          )
+                        ),
+                      ),
                     ),
-                    onPressed: () {},
-                    child: Text("Login", style: TextStyle(color: Colors.black, fontSize: 18)),
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                    child: Opacity(
+                      opacity: 0.4,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          fillColor: Colors.grey[100],
+                          filled: true,
+                          hintText: 'Password',
+                            hintStyle: TextStyle(color: Colors.grey[800]),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5),
+                            borderSide: BorderSide.none
+                          )
+                        ),
+                      ),
+                    ),
+                  ),
+                  ButtonTheme(
+                    minWidth: MediaQuery.of(context).size.width,
+                    height: 50,
+                    child: RaisedButton(
+                      elevation: 10,
+                      color: Colors.white,
+                      padding: EdgeInsets.all(10),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5)
+                      ),
+                      onPressed: () {},
+                      child: Text("LOGIN", style: TextStyle(color: Colors.grey[800], fontSize: 18)),
+                    )
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                    child: FlatButton(
+                      onPressed: () {},
+                      child: RichText(
+                        text: new TextSpan(
+                          // Note: Styles for TextSpans must be explicitly defined.
+                          // Child text spans will inherit styles from parent
+                          style: new TextStyle(
+                            fontSize: 15.0,
+                            color: Colors.grey[800],
+                          ),
+                          children: <TextSpan>[
+                            new TextSpan(text: "Forgot Password? "),
+                            new TextSpan(text: 'Reset it here', style: new TextStyle(fontWeight: FontWeight.bold)),
+                          ],
+                        ),
+                      ),
+                    ),
                   )
-                ),
-              ],
+                ],
+              ),
             ),
           ),
           Align(
             alignment: Alignment.bottomCenter,
             child: Container(
-              child: FlatButton(
-                onPressed: () {},
-                color: Colors.blue[300],
-                padding: EdgeInsets.all(10),
-                child: Text("Don't have an account? Sign up", style: TextStyle(color: Colors.black, fontSize: 18)),
+              child: Opacity(
+                opacity: 0.5,
+                child: FlatButton(
+                  minWidth: MediaQuery.of(context).size.width,
+                  height: 65,
+                  onPressed: () {},
+                  color: Colors.blue[200],
+                  padding: EdgeInsets.all(10),
+                  child: RichText(
+                    text: new TextSpan(
+                      // Note: Styles for TextSpans must be explicitly defined.
+                      // Child text spans will inherit styles from parent
+                      style: new TextStyle(
+                        fontSize: 18.0,
+                        color: Colors.black,
+                      ),
+                      children: <TextSpan>[
+                        new TextSpan(text: "Don't have an account? "),
+                        new TextSpan(text: 'Sign up', style: new TextStyle(fontWeight: FontWeight.bold)),
+                      ],
+                    ),
+                  ),
+                ),
               ),
             ),
           )
